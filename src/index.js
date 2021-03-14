850232de81f7177111ce9e70cff26fa4f515f352
Julia Sen <julia.snk@hotmail.com>
2021-03-14 18:21:51 +0300

    try to solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let map = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '|': '|'
+};
+
+let stack = [];
+
+
+
+for (let i = 0; i < str.length; i++) {
+  if(str[i] === '|'){
+    if(!stack.includes(str[i])){
+      stack.push(str[i]);
+      continue;
+    }else {
+      let last = stack.pop();
+       if (str[i] !== map[last]) {
+         return false
+         }
+         return true;
+      }
+      
+  }
+    
+    if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
+  
+        stack.push(str[i]);
+    }else{
+      let last = stack.pop();
+       if (str[i] !== map[last]) {
+         return false
+         }
+      }
+}
+      
+return stack.length === 0;
 }
